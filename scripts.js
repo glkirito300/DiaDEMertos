819268ea4348324c16a02238289672a0a27de8a3
Binariokalwer <binariokalwer@gmail.com>
2021-09-27 00:05:10 -0500

    actualización_27_09_2021_12_05Da

--- scripts.js
+++ scripts.js
@@ -53,5 +53,54 @@ function comentar(numero){
 }
 function iniciar(){
     var nombre_persona = document.getElementById("nombre").value;
-    speechSynthesis.speak(new SpeechSynthesisUtterance("Bienvenido o bienvenido"+nombre_persona + "te quiero contar una historia"))
+    speechSynthesis.speak(new SpeechSynthesisUtterance("Bienvenido o bienvenido"+nombre_persona + "en este apartado de ayudare a entender mejor el altar de muertos, en esta parte de la pagina hay 4 botones que te diran información sin embargo, hay una trampa y una solución que desactiva la trampa, te dare una pista, siempre esta en ti necesitas de el para vivir, e incluso haces todo con el"))
+}
+var boton1_de_mover = document.getElementById("boton1")
+var boton2_de_mover = document.getElementById("boton2")
+var boton3_de_mover = document.getElementById("boton3")
+var boton4_de_mover = document.getElementById("boton4")
+var body = document.getElementById("fondo")
+var activado = 0;
+var btn1 = 0;
+var btn2 = 0;
+var btn3 = 0;
+var btn4 = 0;
+function mover(numero){
+    if(numero == 1 && activado == 0){
+        if(btn1 == 0){
+            boton1_de_mover.style.left="70%"
+            boton1_de_mover.style.rotate="15deg"
+            btn1 = 1;
+        }else if(btn1 == 1){
+            boton1_de_mover.style.left="0%"
+            btn1 = 0;
+        }
+    }else if(numero == 2 && activado == 0){
+        if(btn2 == 0){
+            boton2_de_mover.style.left="50%"
+            btn2 = 1;
+        }else if(btn2 == 1 && activado == 0){
+            boton2_de_mover.style.left="0%"
+            btn2 = 0;
+        }
+    }else if(numero == 3 && activado == 0){
+        if(btn3 == 0 && activado == 0){
+            boton3_de_mover.style.left="90%"
+            btn3 = 1;
+        }else if(btn3 == 1 && activado == 0){
+            boton3_de_mover.style.left="0%"
+            btn3 = 0;
+        }
+    }else if(numero == 4 && activado == 0){
+        if(btn4 == 0 && activado == 0){
+            boton4_de_mover.style.left="30%"
+            btn4 = 1;
+        }else if(btn4 == 1 && activado == 0){
+            boton4_de_mover.style.left="0%"
+            btn4 = 0;
+        }
+    }
+}
+function desactivar(){
+    activado = 1;
 }
